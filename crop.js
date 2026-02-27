6c7e58e2ce83743556aa0de629b010ee46b937f5
Aayush Sharma <superaayush@Aayushs-MacBook-Pro.local>
2026-02-27 19:48:14 +0530

    add favicon

---
+++ crop.js
@@ -0,0 +1,8 @@
+const sharp = require('sharp');
+sharp('dt-logo.png')
+  .trim({ background: {r:255, g:255, b:255, alpha:1}, threshold: 25 })
+  .resize(180, 180)
+  .png({ compressionLevel: 9, quality: 80 })
+  .toFile('src/app/apple-icon.png')
+  .then(info => console.log('Apple icon created:', info))
+  .catch(err => console.error('Error cropping:', err));
